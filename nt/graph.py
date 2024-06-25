cd53159f775975afb73645c4964145d70e471ee2
AkiraShougun <153398075+AkiraShougun@users.noreply.github.com>
2024-06-25 16:02:52 -0400

    added prime pics

--- nt/graph.py
+++ nt/graph.py
@@ -1,24 +1,21 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
 def division_graph(m):
     direction = []
-    G = nx.cycle_graph(m)
+    G = nx.empty_graph(m)
     pos = nx.circular_layout(G)
     for i in range(m):
         direction.append((i*10)%m)
         G.add_edge(i,direction[i])
     nx.draw(G, pos, with_labels=True)
-    plt.savefig("circular_graph.png")
+    plt.savefig(f"./nt/graph_img/{m}.png")
+    plt.clf()
+    plt.close()
 
 
-# Add edges between specific nodes
-#G.add_edge(1, 1)
-#G.add_edge(4, 1)
+for i in primes:
+   division_graph(i)
 
-# Draw the graph
-
-# Save the figure as an image file
-
-
-division_graph(10)
